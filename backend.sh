4df1911af909e9e865a3c5cdfa7cd0ad97911b99
pavan reddy <gvpavanreddy1996@gmail.com>
2025-01-07 22:53:02 +0530

    enpense-shell

---
+++ backend.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP= $(date +%Y-%m-%d-%H_%M_%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+        echo -e "$1  ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e " $2... $G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute the scripts"
+        exit 1 # other than o
+    fi
+}
+
+echo "Scripts started executing at : $TIMSTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+dnf module disable nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "disable existing default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE_NAME
+VALIDATE $? "Enable nodejs 20"
+
+dnf install nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "Install NodeJs"
+
+useradd expense &>>$LOG_FILE_NAME
+VALIDATE $? "Adding Expense user"
+
+mkdir /app &>>$LOG_FILE_NAME
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Dowaliding backend"
+
+cd /app
+
+unzip /tmp/backend.zip &>>$LOG_FILE_NAME
+VALIDATE $? "UnZip backend"
+
+npm install &>>$LOG_FILE_NAME
+VALIDATE $? "Installing dependencies"
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+
+# prepare mysql schema
+
+dnf install mysql -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing mysql client"
+
+mysql -h mysql.pavanreddy.store -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+VALIDATE $? "Setting up the transcations schema and tables"
+
+systemctl daemon-reload &>>$LOG_FILE_NAME
+VALIDATE $? "Baemon Reload"
+
+systemctl start backend &>>$LOG_FILE_NAME
+VALIDATE $? "Starting backend"
+
+systemctl enable backend &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling backend"
